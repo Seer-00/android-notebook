cda5c068fd442915799ff26779973110c72ee973
XI <2298968255@qq.com>
2019-12-13 19:10:33 +0800

    Commit from XI 131909

--- app/src/main/java/com/example/project1213/AddRecords.java
+++ app/src/main/java/com/example/project1213/AddRecords.java
@@ -3,6 +3,8 @@ package com.example.project1213;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddRecords extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class AddRecords extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_records);
+        Button addRecord=(Button)findViewById(R.id.send);
+        addRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/project1213/Main2Activity.java
+++ app/src/main/java/com/example/project1213/Main2Activity.java
@@ -3,17 +3,29 @@ package com.example.project1213;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class Main2Activity extends AppCompatActivity {
+
+public class Main2Activity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+
+    private RadioGroup radioGroup;
+    private RadioButton find,mine;
+    private MyFragment findFragment,mineFragment;
+    boolean mark;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        mark=false;
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle("Traveller");
         toolbar.inflateMenu(R.menu.operation);
@@ -22,7 +34,7 @@ public class Main2Activity extends AppCompatActivity {
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.add:
-                        startActivity(new Intent(Main2Activity.this,AddRecords.class));
+                            startActivity(new Intent(Main2Activity.this,AddRecords.class));
                         break;
                     case R.id.about:
                         startActivity(new Intent(Main2Activity.this,About.class));
@@ -31,6 +43,51 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+
+        radioGroup=(RadioGroup)findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        find=(RadioButton)findViewById(R.id.find);
+        mine=(RadioButton)findViewById(R.id.mine);
+
+        find.setChecked(true);
+
+
+    }
+
+    public void hideAllFragment(FragmentTransaction transaction){
+        if(findFragment!=null){
+            transaction.hide(findFragment);
+        }
+        if(mineFragment!=null){
+            transaction.hide(mineFragment);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group,int checkedId) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        hideAllFragment(transaction);
+        switch (checkedId){
+            case R.id.find:
+                if(findFragment==null){
+                    findFragment=new MyFragment("发现");
+                    transaction.add(R.id.fl,findFragment);
+                }else{
+                    transaction.show(findFragment);
+                }
+                break;
+            case R.id.mine:
+                    if(mineFragment==null){
+                       mineFragment=new MyFragment("我的");
+                       transaction.add(R.id.fl,mineFragment);
+                    }else{
+                        transaction.show(mineFragment);
+                    }
+
+                break;
+        }
+        transaction.commit();
     }
 }
 
